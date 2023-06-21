20a7a07f2aff835d3f0d9211d26aca9308d3a553
Marco Peressutti <marco.eugenio.peressutti@gmail.com>
2023-06-21 19:02:23 +0200

    [mapgen] Fixed bug with Mapgen
    > Now the map.sdf will not be pushed to the repo
    > Fixed bug with MapGen AppImage
    > Added some comments in script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,18 @@
+# display line and print errors
 set -xe
 
-colcon build
-source ./install/setup.sh
-
-./utils/MapGen-x86_64.AppImage
-
-mv ./conf ./utils/
-mv ./map.sdf ./utils/
-
+# variables
 conf='./utils/conf'
 
+# map generation sub routine
+./utils/MapGen-x86_64.AppImage
+
+# mv files in the code base
+mv ./conf ./utils/
+mv ./map.sdf ./utils/
 cp ./utils/map.sdf ./src/simulator/models/mindstorm_map/
 
+# build source and run
+colcon build
+source ./install/setup.sh
 ros2 launch simulator sim.launch.py $(<"$conf")
