5e9914a2e2edcca3fb0c3e33b03f23a3acbf20f6
Marco Peressutti <marco.eugenio.peressutti@gmail.com>
2023-06-22 17:51:59 +0200

    [devtool] Automated development commands
    > Renamed script.sh to devtool.sh
    > Implemented autocompletion of rpl console command
    > Added basic description of rpl console commands to README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # robot-planning-ros2
+
+## devtool.sh
+In order to get access to the development tools and shortcuts to build, run and generate maps:
+
+```console
+$ cd robot-planning-ros2
+$ source ./devtool.sh
+$ rpl
+Available commands
+
+	 rpl clean  : removes install, log, build and files in utils/
+	 rpl build  : performs build packages and source environment
+	 rpl mapgen : calls MapGen to generate custom map
+	 rpl run    : run simulation
+	 rpl all    : clean -> mapgen -> build -> run
+```
---
+++ devtool.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+function rpl(){
+    # display errors
+    command="$(echo "$1" | tr '[:upper:]' '[:lower:]')"
+
+    case $command in
+        clean)
+            rm -rf ./install ./build ./log
+            rm -rf ./utils/conf ./utils/map.sdf
+            ;;
+        build)
+            colcon build
+            source ./install/setup.sh
+            ;;
+        mapgen)
+            ./utils/MapGen-x86_64.AppImage
+            if [[ -f ./conf ]]; then
+                mv ./conf ./utils/
+                mv ./map.sdf ./utils/
+                cp ./utils/map.sdf ./src/simulator/models/mindstorm_map/
+                conf='./utils/conf'
+            fi
+            ;;
+        run)
+            colcon build
+            source ./install/setup.sh
+            if [[ -f ./utils/conf ]]; then
+                ros2 launch simulator sim.launch.py $(<"$conf")
+            else 
+                ros2 launch shelfino_gazebo multi_shelfino.launch.py
+            fi
+            ;;
+        all)
+            rpl clean
+            rpl mapgen
+            rpl build
+            rpl run
+            ;;
+        shortlist)
+            echo clean build mapgen run all
+            ;;
+        *)
+            echo "Available commands"
+            echo ""
+            echo -e "\t rpl clean  : removes install, log, build and files in utils/"
+            echo -e "\t rpl build  : performs build packages and source environment"
+            echo -e "\t rpl mapgen : calls MapGen to generate custom map"
+            echo -e "\t rpl run    : run simulation"
+            echo -e "\t rpl all    : clean -> mapgen -> build -> run"
+        ;;
+    esac
+}
+
+function _rpl() {
+    local AVAILABLE_COMMANDS=$(rpl shortlist)
+    COMPREPLY=()
+    if [ "$COMP_CWORD" -eq 1 ] ; then
+        local cur=${COMP_WORDS[COMP_CWORD]}
+        COMPREPLY=($( compgen -W "$AVAILABLE_COMMANDS" -- $cur ))
+    fi
+
+}
+
+complete -o nospace -F _rpl rpl
